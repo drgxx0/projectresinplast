86f0226fc7d77d402269d50e051308b1dd2cc014
unknown <alejandroff91@gmail.com>
2019-03-08 22:47:53 -0500

    cambios

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/projectresinplast/precache-manifest.2e9f3d3217d5fb649a65886509c21771.js"
+  "/projectresinplast/precache-manifest.6990a61dcaf9263209c85ff1d2ec8c6a.js"
 );
 
 workbox.clientsClaim();
